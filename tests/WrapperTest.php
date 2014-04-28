6ee24502dfa0b57747b6c23382d96f1ed15e4d11
Kamil Piechaczek <pomek@desmart.com>
2014-04-28 14:56:41 +0200

    "panie, daj pan spokoj"... (poprawki MR)

--- tests/WrapperTest.php
+++ tests/WrapperTest.php
@@ -11,72 +11,72 @@ class WrapperTest extends \PHPUnit_Framework_TestCase {
   }
 
   public function testResize() {
-    $factory = $this->factory()->resize(51, 49);
+    $wrapper = $this->factory()->resize(51, 49);
 
-    $this->assertEquals($factory->toString(), "http://dms.local.com/ResizeImage/images/srpr/51,49,100,3,_logo.png");
+    $this->assertEquals($wrapper->toString(), "http://dms.local.com/ResizeImage/images/srpr/51,49,100,3,_logo.png");
   }
 
   public function testQuality() {
-    $factory = $this->factory()->quality(39);
+    $wrapper = $this->factory()->quality(39);
 
-    $this->assertEquals($factory->toString(), "http://dms.local.com/ResizeImage/images/srpr/0,0,39,3,_logo.png");
+    $this->assertEquals($wrapper->toString(), "http://dms.local.com/ResizeImage/images/srpr/0,0,39,3,_logo.png");
   }
 
   public function testCrop() {
-    $factory = $this->factory()->crop();
+    $wrapper = $this->factory()->crop();
 
-    $this->assertEquals($factory->toString(), "http://dms.local.com/ResizeImage/images/srpr/0,0,100,3,crop-1_logo.png");
+    $this->assertEquals($wrapper->toString(), "http://dms.local.com/ResizeImage/images/srpr/0,0,100,3,crop-1_logo.png");
   }
 
   public function testSharpen() {
-    $factory = $this->factory('logo.jpg')->sharpen();
+    $wrapper = $this->factory('logo.jpg')->sharpen();
 
-    $this->assertEquals($factory->toString(), "http://dms.local.com/ResizeImage/images/srpr/0,0,100,2,sharpen-1_logo.jpg");
+    $this->assertEquals($wrapper->toString(), "http://dms.local.com/ResizeImage/images/srpr/0,0,100,2,sharpen-1_logo.jpg");
   }
 
   public function testKeepRatio() {
-    $factory = $this->factory('logo.jpg')->keepRatio();
+    $wrapper = $this->factory('logo.jpg')->keepRatio();
 
-    $this->assertEquals($factory->toString(), "http://dms.local.com/ResizeImage/images/srpr/0,0,100,2,keepratio-1_logo.jpg");
+    $this->assertEquals($wrapper->toString(), "http://dms.local.com/ResizeImage/images/srpr/0,0,100,2,keepratio-1_logo.jpg");
   }
 
   public function testMaxWh() {
-    $factory = $this->factory()->maxWh();
+    $wrapper = $this->factory()->maxWh();
 
-    $this->assertEquals($factory->toString(), "http://dms.local.com/ResizeImage/images/srpr/0,0,100,3,maxwh-1_logo.png");
+    $this->assertEquals($wrapper->toString(), "http://dms.local.com/ResizeImage/images/srpr/0,0,100,3,maxwh-1_logo.png");
   }
 
   public function testResizeAndCrop() {
-    $factory = $this->factory()->resize(51, 49);
-    $factory->crop();
+    $wrapper = $this->factory()->resize(51, 49);
+    $wrapper->crop();
 
-    $this->assertEquals($factory->toString(), "http://dms.local.com/ResizeImage/images/srpr/51,49,100,3,crop-1_logo.png");
+    $this->assertEquals($wrapper->toString(), "http://dms.local.com/ResizeImage/images/srpr/51,49,100,3,crop-1_logo.png");
   }
 
   public function testKeepRatioAndSharpen() {
-    $factory = $this->factory('logo.gif')->keepRatio()
-        ->sharpen();
+    $wrapper = $this->factory('logo.gif')->keepRatio()
+      ->sharpen();
 
-    $this->assertEquals($factory->toString(), "http://dms.local.com/ResizeImage/images/srpr/0,0,100,1,sharpen-1;keepratio-1_logo.gif");
+    $this->assertEquals($wrapper->toString(), "http://dms.local.com/ResizeImage/images/srpr/0,0,100,1,sharpen-1;keepratio-1_logo.gif");
   }
 
   public function testKeepRatioAndCropAndSharpenAndMaxWh() {
-    $factory = $this->factory()->keepRatio()
+    $wrapper = $this->factory()->keepRatio()
       ->crop()
       ->sharpen()
       ->maxWh();
 
-    $this->assertEquals($factory->toString(), "http://dms.local.com/ResizeImage/images/srpr/0,0,100,3,crop-1;sharpen-1;keepratio-1;maxwh-1_logo.png");
+    $this->assertEquals($wrapper->toString(), "http://dms.local.com/ResizeImage/images/srpr/0,0,100,3,crop-1;sharpen-1;keepratio-1;maxwh-1_logo.png");
   }
 
   public function testCropAndSharpenAndMaxWhAndResizeAndQuality() {
-    $factory = $this->factory()->crop()
-        ->sharpen()
-        ->maxWh()
-        ->resize(19, 81)
-        ->quality(15);
+    $wrapper = $this->factory()->crop()
+      ->sharpen()
+      ->maxWh()
+      ->resize(19, 81)
+      ->quality(15);
 
-    $this->assertEquals($factory->toString(), "http://dms.local.com/ResizeImage/images/srpr/19,81,15,3,crop-1;sharpen-1;maxwh-1_logo.png");
+    $this->assertEquals($wrapper->toString(), "http://dms.local.com/ResizeImage/images/srpr/19,81,15,3,crop-1;sharpen-1;maxwh-1_logo.png");
   }
 
   /**
